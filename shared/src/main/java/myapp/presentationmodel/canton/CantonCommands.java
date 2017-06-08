6b7bbc5edc99d4d1f7b42c80007c5d24b7128c37
Fabian Bissig <fabian.bissig@students.fhnw.ch>
2017-06-08 11:58:54 +0200

    1. version

---
+++ shared/src/main/java/myapp/presentationmodel/canton/CantonCommands.java
@@ -0,0 +1,12 @@
+package myapp.presentationmodel.canton;
+
+public interface CantonCommands {
+	String LOAD_SOME_CANTON = unique("loadSomePCanton");
+	String SAVE             = unique("save");
+	String RESET            = unique("reset");
+
+	static String unique(String key) {
+		return CantonCommands.class.getName() + "." + key;
+	}
+
+}
